d4917bcb78be3adc0bc554367252342f20df0387
З - 6 < - 6@DESKTOP-Q5FB80V>
2024-07-15 22:01:48 +0300

    Добавьте файлы проекта.

---
+++ FileName.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include "Stack.hpp"
+#include "Queue.hpp"
+
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+	//Стек
+	// filo - первый зашел послений вышел
+	
+	Stack<int> stack(3);
+	
+	std::cout << stack.isEmpty() << std::endl; //1 пустой стэк
+	//std::cout << stack.isFull() << std::endl;//проверка полный ли стэк
+	stack.put(7);
+	stack.put(50);
+	stack.put(150);
+	
+	std::cout << stack.isEmpty() << std::endl; //0 не пустой стэк
+	std::cout << stack.get() << std::endl;
+	std::cout << stack.get() << std::endl;
+	std::cout << stack.get() << std::endl;
+	std::cout << stack.isEmpty() << std::endl;
+
+	if(!stack.isEmpty())
+	std::cout << stack.get() << std::endl;
+	
+	// Структура данных (очередь)
+	//fifo - первый вошел первый вышел
+
+	Queue<int, 3> queue;
+	queue.put(10);
+	queue.put(20);
+	std::cout << queue.get() << std::endl; //10
+	queue.put(30);
+	std::cout << queue.get() << std::endl; //20
+	queue.clear();
+	std::cout << queue.get() << std::endl;
+
+	return 0;
+}
---
+++ Stack.hpp
@@ -0,0 +1,84 @@
+﻿#ifndef _STACK_HPP_
+#define _STACK_HPP_
+
+#include <iostream>
+#include <exception>
+
+
+template <typename ANY>
+class Stack {
+private:
+	ANY* first;
+	ANY* last;
+	size_t size; // максимальный размкер
+	size_t elements; //проверка количества элементов на данный момент
+public:
+	Stack(size_t size); //параметрический
+	~Stack();
+	void put(ANY value); //put метод позволяет добасить элемент
+	ANY get();
+	ANY look();// метод
+	void clear();// метод удаляет все элементы из очереди
+	bool isFull(); //метод проверить не пустой ли стек
+	bool isEmpty(); //метод логического значения пишется так is...
+	void upgrade(); //метоод увеличение стэка если нужно
+};
+
+template <typename ANY>  // описание методов
+Stack<ANY>::Stack(size_t size) {
+	if (size == 0)
+		throw std::exception("null size error"); //вызов ошибки
+	first = new ANY[size] {};
+	last = nullptr; //последний элемент, на данном этапе не понятно какой по счету
+	this->size = size; 
+	elements = 0;
+}
+
+template <typename ANY>
+Stack<ANY>::~Stack() { 
+	delete[] first;
+}
+
+template <typename ANY>
+void Stack<ANY>::put(ANY value) { //метод для добавления элементов в стэк
+	if(isFull())
+		throw std::exception("stack overflow"); // перед добавлением элемента, если стэк полный то вызываем ошибку
+	
+	last = &(first[elements++] = value);//вариант сложнее
+	
+	//first[elements++] = value;
+	//last = &first[elements - 1] // направление на последний элемент простой вариант	
+}
+
+template <typename ANY>
+ANY Stack<ANY>::get() { //дает значение последнего элемента в стеке и удаляет
+	if (isEmpty())
+		throw std::exception("stack isEmpty");
+
+	--elements;
+	return *(last--);
+}
+
+template <typename ANY>
+ANY Stack<ANY>::look() { //дает значение последнего элемента в стеке
+	if (isEmpty())
+		throw std::exception("stack isEmpty");
+	return *last;
+}
+
+template <typename ANY>
+bool Stack<ANY>::isEmpty() {
+	return elements == 0;
+}
+
+template <typename ANY>
+bool Stack<ANY>::isFull() {
+	return elements == size;
+}
+
+template <typename ANY>
+void Stack<ANY>::clear() { // очещение элементов в стеке
+	elements = 0;
+}
+
+#endif //_STACK_HPP_
